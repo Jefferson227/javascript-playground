f016eafc5ee5f1184d5243d0db6ddae1ae8fac06
Jefferson Matheus <jefferson227@gmail.com>
2017-12-23 10:59:29 -0300

    chore(rxjs): introduce callbacks

--- rxjs/observables.js
+++ rxjs/observables.js
@@ -1,1 +1,25 @@
-console.log('Hello world');
+/**
+ * Just practicing my knowledge on RxJs observables.
+ * Inspired by André Staltz video here: https://www.youtube.com/watch?v=uQ1zhJHclvs
+ */
+
+let arr = [10, 20, 30];
+
+function next(ob) {
+    console.log(ob);
+}
+
+function error(ob) {
+    console.error(ob);
+}
+
+function done() {
+    console.log('done');
+}
+
+function getMeSomeData(nextCb, errorCd, doneCb) {
+    arr.forEach(nextCb);
+    doneCb();
+}
+
+getMeSomeData(next, error, done);
